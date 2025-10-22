d65deb81d27ea0e63c4bdcc7159e8d06c10ccab1
claxn <clax.n@gmx.at>
2025-10-22 23:27:30 +0000

    modified init imports

--- src/eodc_cf/__init__.py
+++ src/eodc_cf/__init__.py
@@ -1,5 +1,5 @@
 from eodc_cf._core import CFDataVariable, CFDataset, CFCoordinate
-from eodc_cf.lib import CFLatCoordinate, CFLonCoordinate, CFXCoordinate, CFYCoordinate, CFTimeCoordinate
+#from eodc_cf.lib import CFLatCoordinate, CFLonCoordinate, CFXCoordinate, CFYCoordinate, CFTimeCoordinate
 from eodc_cf._version import __version__
 from eodc_cf._version import __commit__
 
