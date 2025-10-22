4c48ee08b976c97e536efc62c0afbf785dc288a9
claxn <clax.n@gmx.at>
2025-10-22 23:09:44 +0000

    fixed version import

--- src/eodc_cf/__init__.py
+++ src/eodc_cf/__init__.py
@@ -1,4 +1,6 @@
 from _core import CFDataVariable, CFDataset, CFCoordinate
 from lib import CFLatCoordinate, CFLonCoordinate, CFXCoordinate, CFYCoordinate, CFTimeCoordinate
+from eodc_cf._version import __version__
+from eodc_cf._version import __commit__
 
 __all__ = CFCoordinate + CFDataVariable + CFDataset + CFLatCoordinate + CFLonCoordinate + CFXCoordinate + CFYCoordinate + CFTimeCoordinate
